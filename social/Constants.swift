0e25f139bfbf83f396b5f3a642699e951aa2410c
raj-subhankar <subhankar.rj@gmail.com>
2017-05-27 21:40:37 +0530

    Sign in ui custom classes

---
+++ social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  social
+//
+//  Created by Subhankar Dee Raj on 27/05/17.
+//  Copyright © 2017 Subhankar. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ social/FancyView.swift
@@ -0,0 +1,23 @@
+//
+//  FancyView.swift
+//  social
+//
+//  Created by Subhankar Dee Raj on 27/05/17.
+//  Copyright © 2017 Subhankar. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        
+    }
+
+}
