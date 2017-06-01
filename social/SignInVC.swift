486b94b9fd74182bbdf6e6d71f270366bd36e05a
raj-subhankar <subhankar.rj@gmail.com>
2017-06-01 13:25:42 +0530

    Complete email auth

--- social/SignInVC.swift
+++ social/SignInVC.swift
@@ -12,6 +12,10 @@ import FBSDKCoreKit
 import Firebase
 
 class SignInVC: UIViewController {
+    
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var passwordField: FancyField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,5 +56,24 @@ class SignInVC: UIViewController {
         })
     }
 
+    @IBAction func signInTapped(_ sender: Any) {
+        
+        if let email = emailField.text, let pwd = passwordField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("SignInVC: Email user authenticated with firebase")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("SignInVC: Unable to authenticate with firebase using email")
+                        } else {
+                            print("Successfully authenticated using firebase")
+                        }
+                    })
+                }
+            })
+        }
+        
+    }
 }
 
