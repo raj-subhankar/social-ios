397773a7edc96c43fc24f1d0ff2ed3a412797365
raj-subhankar <subhankar.rj@gmail.com>
2017-06-08 13:44:15 +0530

    Setup feed table

---
+++ social/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  social
+//
+//  Created by Subhankar Dee Raj on 08/06/17.
+//  Copyright © 2017 Subhankar. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}
